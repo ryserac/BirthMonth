044e2a640dde5b3d086c2cd25a80501c382f985c
ryserac <ryserac@mail.uc.edu>
2023-06-12 23:21:26 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int birthMonth = 13;
+        if ((birthMonth >= 1) & (birthMonth <= 12)) {
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        else {
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
+    }
+}
